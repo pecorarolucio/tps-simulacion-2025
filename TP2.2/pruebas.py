4edfbadbd7210ce10e98866285ba542c0bcbb8c3
pecorarolucio <luciopecoraro2002@gmail.com>
2025-05-18 20:01:56 -0300

    agrego histogramas

--- TP2.2/pruebas.py
+++ TP2.2/pruebas.py
@@ -1,9 +1,11 @@
 import random
-import matplotlib as mp
-import scipy.stats
 import math
+import matplotlib.pyplot as plt
 
-def distr_uniforme(a, b, size=1):
+#Valor n
+n=10000
+
+def distr_uniforme(a, b, size):
     """
     Distribucion uniforme
     a, b: rango de valores
@@ -13,13 +15,35 @@ def distr_uniforme(a, b, size=1):
         x.append(a+(b-a)*random.random())
     return x
 
-def distr_exp(ex, size=1):
+# Test: generar 10.000 números entre 5 y 15
+datos_uniforme = distr_uniforme(5, 15, n)
+
+# Graficar histograma
+plt.hist(datos_uniforme, bins=50, density=True, edgecolor='black')
+plt.title('Distribución Uniforme Continua U(5, 15)')
+plt.xlabel('Valor')
+plt.ylabel('Densidad')
+plt.grid(True)
+plt.show()
+
+def distr_exp(ex, size):
     x=[]
     for _ in range(size):
         x.append(-ex*math.log(random.random()))
     return x
 
-def distr_gamma(k, a, size=1):
+# Test: generar 10.000 números con lambda = 1.5
+datos_exponencial = distr_exp(1.5, n)
+
+# Graficar histograma
+plt.hist(datos_exponencial, bins=50, density=True, edgecolor='black')
+plt.title('Distribución Exponencial λ = 1.5')
+plt.xlabel('Valor')
+plt.ylabel('Densidad')
+plt.grid(True)
+plt.show()
+
+def distr_gamma(k, a, size):
     """
     Generador de valores con distribucion gamma basado en el libro
     k, a: parametros necesarios para distribucion gamma
@@ -32,7 +56,19 @@ def distr_gamma(k, a, size=1):
         x.append(-(math.log(tr)/a))
     return x
 
-def distr_normal(ex, stdx, size=1):
+# Test: generar 10.000 números con alpha = 3, beta = 2
+datos_gamma = distr_gamma(3, 2, n)
+
+# Graficar histograma
+plt.hist(datos_gamma, bins=50, density=True, edgecolor='black')
+plt.title('Distribución Gamma α = 3, β = 2')
+plt.xlabel('Valor')
+plt.ylabel('Densidad')
+plt.grid(True)
+plt.savefig('gamma.png')
+plt.show()
+
+def distr_normal(ex, stdx, size):
     x=[]
     for _ in range(size):
         sum=0
@@ -40,6 +76,19 @@ def distr_normal(ex, stdx, size=1):
             sum=sum+random.random()
         x.append(stdx*(sum-6)+ex)
     return x
+
+# Test: generar 10.000 números con media 0 y desviación 1
+datos_normal = distr_normal(0, 1, n)
+
+# Graficar histograma
+plt.hist(datos_normal, bins=50, density=True, edgecolor='black')
+plt.title('Distribución Normal N(0, 1)')
+plt.xlabel('Valor')
+plt.ylabel('Densidad')
+plt.grid(True)
+plt.savefig('normal.png')
+plt.show()
+
 
 def distr_pascal(k, q, size=1): #en el libro esta como binomial negativa, es un caso especial de esa
     nx=[]
@@ -51,7 +100,7 @@ def distr_pascal(k, q, size=1): #en el libro esta como binomial negativa, es un 
         nx.append(x)
     return nx
 
-def distr_binomial(n, p, size=1):
+def distr_binomial(n, p, size):
     x=[]
     for _ in range(size):
         y=0
@@ -60,6 +109,18 @@ def distr_binomial(n, p, size=1):
                 y+=1
         x.append(y)
     return x
+
+# Test: 10000 muestras con n = 10, p = 0.3
+datos_binomial = distr_binomial(10, 0.3, n)
+
+# Histograma
+plt.hist(datos_binomial, bins=range(0, 11), density=True, edgecolor='black', align='left')
+plt.title('Distribución Binomial n=10, p=0.3')
+plt.xlabel('Cantidad de éxitos')
+plt.ylabel('Probabilidad')
+plt.grid(True)
+plt.savefig('binomial.png')
+plt.show()
 
 def distr_hipergeometrica(tn, ns, p, size=1):
     x=[]
@@ -79,7 +140,7 @@ def distr_hipergeometrica(tn, ns, p, size=1):
         x.append(y)
     return x
 
-def distr_poisson(p, size=1): #p seria el lambda
+def distr_poisson(p, size): #p seria el lambda
     x=[]
     for _ in range(size):
         y=0
@@ -92,6 +153,18 @@ def distr_poisson(p, size=1): #p seria el lambda
                 y+=1
         x.append(y)
     return x
+
+# Test: generar 10.000 valores con lambda = 4
+datos_poisson = distr_poisson(4, n)
+
+# Histograma
+plt.hist(datos_poisson, bins=range(0, max(datos_poisson)+1), density=True, edgecolor='black', align='left')
+plt.title('Distribución Poisson λ = 4')
+plt.xlabel('Número de eventos')
+plt.ylabel('Probabilidad')
+plt.grid(True)
+plt.savefig('poisson.png')
+plt.show()
 
 def distr_empdiricadiscr(size=1): #empirica discreta
     x = []
